ce514006f4ec6daaf33f073f90f72ae059ced4f9
anirudh <anirudhsingh3019@gmail.com>
2025-03-22 13:47:08 +0530

    Added Earliest Deadline First (EDF)

---
+++ algorithms/edf.py
@@ -0,0 +1,31 @@
+import heapq
+from utils.process import Process
+
+def earliest_deadline_first(processes):
+    processes.sort(key=lambda p: p.arrival_time)
+    ready_queue = []
+    timeline = []
+    time, index, completed = 0, 0, 0
+
+    while completed < len(processes):
+        while index < len(processes) and processes[index].arrival_time <= time:
+            heapq.heappush(ready_queue, (processes[index].deadline, processes[index].pid, processes[index]))
+            index += 1
+
+        if ready_queue:
+            _, _, p = heapq.heappop(ready_queue)
+            if p.start_time == -1:
+                p.start_time = time
+                p.response_time = time - p.arrival_time
+
+            time += p.burst_time
+            p.completion_time = time
+            p.turnaround_time = p.completion_time - p.arrival_time
+            p.waiting_time = p.turnaround_time - p.burst_time
+            timeline.append((p.pid, p.burst_time))
+            completed += 1
+        else:
+            timeline.append(("Idle", 1))
+            time += 1
+
+    return processes, timeline
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from utils.visualization import draw_gantt_chart
 from algorithms.fcfs import fcfs
 from algorithms.prio import priority_scheduling
 from algorithms.rr import round_robin
+from algorithms.edf import earliest_deadline_first
 def get_processes(priority=False, edf=False):
     processes = []
     n = int(input("Enter the number of processes: "))
